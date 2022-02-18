b2048caccc27c36013972613f264b13435d811b9
Layne <3378830682@qq.com>
2022-02-18 16:15:03 +0800

    hotfix commit1

--- src/main/java/com/cqust/git/GitTest.java
+++ src/main/java/com/cqust/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello Git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello git45");
     }
 }
