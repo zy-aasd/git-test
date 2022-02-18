39320ed7308fd7936e64677c3fc08dcd2cedcb3a
Layne <3378830682@qq.com>
2022-02-18 15:59:50 +0800

    first commit

---
+++ src/main/java/com/cqust/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cqust.git;
+
+/**
+ * @author ZhangYang
+ * @date 2022/2/18 15:55
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
