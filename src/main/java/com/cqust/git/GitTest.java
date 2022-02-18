e439d9193cbfe9caa94f9c1055c0369ceaac6a7b
Layne <3378830682@qq.com>
2022-02-18 16:03:01 +0800

    second commit

--- src/main/java/com/cqust/git/GitTest.java
+++ src/main/java/com/cqust/git/GitTest.java
@@ -7,5 +7,6 @@ package com.cqust.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello git2");
     }
 }
